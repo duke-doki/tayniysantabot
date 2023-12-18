fd7e20b1dea549706c651dce1ce9b67894575e3d
duke-doki <145125297+duke-doki@users.noreply.github.com>
2023-12-18 15:06:49 +0300

    Update TG_find_winners.py

--- TG_find_winners.py
+++ TG_find_winners.py
@@ -17,30 +17,30 @@ def find_winners():
 
         for party in parties:
             if party.end_of_registration and (party.end_of_registration.strftime('%Y-%m-%d %H:%M:%S') == datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')) and party.winners.count() == 0:
+                if party.players.count() >= 3:
+                    players = [player.username for player in
+                               party.players.all()]
+                    random.shuffle(players)
+                    santas = {}
+                    for username in players:
+                        if not username == players[-1]:
+                            santas[username] = players[players.index(username) + 1]
+                        else:
+                            santas[username] = players[0]
 
-                players = [player.username for player in
-                           party.players.all()]
-                random.shuffle(players)
-                santas = {}
-                for username in players:
-                    if not username == players[-1]:
-                        santas[username] = players[players.index(username) + 1]
-                    else:
-                        santas[username] = players[0]
 
+                    for key, value in santas.items():
+                        gets_gift = Person.objects.get(username=value)
+                        sends_gift = Person.objects.get(username=key)
+                        text = (
+                                Message.objects.get(name='Победитель').text
+                                + '\n'
+                                + f'\nИмя: {gets_gift.name}\n'
+                                + f'\ntelegram: @{gets_gift.username}\n'
+                                + f'\nemail: {gets_gift.email}\n'
+                        )
 
-                for key, value in santas.items():
-                    gets_gift = Person.objects.get(username=value)
-                    sends_gift = Person.objects.get(username=key)
-                    text = (
-                            Message.objects.get(name='Победитель').text
-                            + '\n'
-                            + f'\nИмя: {gets_gift.name}\n'
-                            + f'\ntelegram: @{gets_gift.username}\n'
-                            + f'\nemail: {gets_gift.email}\n'
-                    )
-
-                    Winner.objects.create(text=text, santa=sends_gift, party=party)
+                        Winner.objects.create(text=text, santa=sends_gift, party=party)
     else:
         time.sleep(5)
 
